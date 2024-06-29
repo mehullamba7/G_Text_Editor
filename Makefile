54bf44d651c8a14c8e333c47f3d426788fca8a25
mehullamba7 <mehullamba07@gmail.com>
2024-06-29 14:48:16 +0530

    Added all final files

---
+++ Makefile
@@ -0,0 +1,2 @@
+g: g.c
+	$(CC) g.c -o g -Wall -Wextra -pedantic -std=c99
---
+++ test.c
@@ -0,0 +1,13 @@
+/* Our 'G' editor even has support for various files like .c, .cpp etc.
+  highlighting various keywords, comments , integers, special characters with different colours
+  Let's make a short C program to test this out
+*/
+#include<stdio.h>
+int main(){
+  printf("This editor is a G");
+  return 0;
+}
+
+//This editor really is a G
+//Lets save this file with the name of test.c
+
